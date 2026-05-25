0ea5685a19122b7288da4e3ac0fbad22876c2b12
Nurseit <alaevnurik312@gmail.com>
2026-05-25 17:06:44 +0600

    просто повторил за агаем

--- articles/admin.py
+++ articles/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
+from .models import ToDolist
 
-from articles.models import Article
 
-# Register your models here.
+class ToDolistAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'execution_date']
+    list_filter = ['status']
+    search_fields = ['description']
 
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'author', 'created_at']
-    list_filter = ['author']
-    search_fields = ['title', 'content']
-    fields = ['title', 'author', 'content', 'created_at', 'updated_at']
-    readonly_fields = ['created_at', 'updated_at']
+admin.site.register(ToDolist, ToDolistAdmin)
 
-admin.site.register(Article, ArticleAdmin)
