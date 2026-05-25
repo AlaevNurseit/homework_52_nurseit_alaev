ecbc02449cef22b814cba81fe19ab1ab06d0c881
Nurseit <alaevnurik312@gmail.com>
2026-05-25 17:33:40 +0600

    поменял импорты и urlpatterns

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
-from articles.views import articles, article_create_view, article
+from articles.views import todolist_list, todolist_create, todolist_detail
 
 urlpatterns = [
-    path("todolist/", articles),
-    path("todolist/add/", article_create_view),
-    path("todolist/detail/", article),
+    path("todolist/", todolist_list),
+    path("todolist/add/", todolist_create),
+    path("todolist/detail/", todolist_detail),
 ]
